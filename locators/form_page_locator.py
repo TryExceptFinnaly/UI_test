0fb4bf813e46a05005f9a43c78d2af3648842ffb
TryExceptFinnaly <olegempix@gmail.com>
2022-10-27 20:22:35 +0300

    page object model

---
+++ locators/form_page_locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class FormPageLocators:
+    USER_NAME = (By.CSS_SELECTOR, '#username')
+    USER_PASSWORD = (By.CSS_SELECTOR, '#password')
+    BTN_SUBMIT = (By.TAG_NAME, 'button')
+    LABEL_FIO = (By.CSS_SELECTOR, '#app > div.wrap > div.header > div > div > ul.nav.navbar-nav.pull-right > li.dropdown > div > div.pull-right > div:nth-child(1)')
--- tests/form_test.py
+++ tests/form_test.py
@@ -1,3 +1,4 @@
+from time import sleep
 from pages.form_page import FormPage
 
 class TestFormPage:
@@ -5,3 +6,5 @@ class TestFormPage:
     def test_form(self, driver):
         form_page = FormPage(driver, 'https://nt.ris-x.com/')
         form_page.open()
+        result = form_page.fill_fields_and_submit()
+        assert result == 'Дубровин А. В.'
