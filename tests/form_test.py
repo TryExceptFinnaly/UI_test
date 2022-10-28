f05ddf10bd586990a1ddfbf8dd86691836dad952
TryExceptFinnaly <olegempix@gmail.com>
2022-10-28 14:14:10 +0300

    test commit by pycharm

--- tests/form_test.py
+++ tests/form_test.py
@@ -3,10 +3,8 @@ from pages.form_page import FormPage
 
 
 class TestFormPage:
-
     def test_form(self, driver):
         form_page = FormPage(driver, 'https://nt.ris-x.com/')
         form_page.open()
         result = form_page.fill_fields_and_submit()
         assert result == 'Дубровин А. В.'
-        sleep(5)
